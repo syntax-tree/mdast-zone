e2991843c4baab6479c31ffc43238239713b5267
Titus Wormer <tituswormer@gmail.com>
2021-04-23 19:47:01 +0200

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -1,36 +1,47 @@
 'use strict'
 
+var fs = require('fs')
+var path = require('path')
 var test = require('tape')
 var remark = require('remark')
-var fixtures = require('./fixtures')
+var hidden = require('is-hidden')
 var zone = require('..')
 
 test('mdast-zone', function (t) {
-  fixtures.forEach(each)
+  var root = path.join(__dirname, 'fixtures')
+  var fixtures = fs.readdirSync(root)
+  var index = -1
+  var output
+  var name
+  var test
+
+  while (++index < fixtures.length) {
+    name = fixtures[index]
+    output = null
+
+    if (hidden(name)) continue
+
+    try {
+      output = fs.readFileSync(path.join(root, name, 'output.md'))
+    } catch (_) {}
+
+    test = require(path.join(root, name))
+
+    remark()
+      .use(() => (tree) => {
+        test(t, zone, tree)
+      })
+      .process(
+        fs.readFileSync(path.join(root, name, 'input.md')),
+        (error, file) => {
+          t.ifError(error, 'should not fail (' + name + ')')
+
+          if (output) {
+            t.equal(String(file), String(output), 'should stringify ' + name)
+          }
+        }
+      )
+  }
 
   t.end()
-
-  function each(fixture) {
-    remark().use(plugin).process(fixture.input, done)
-
-    function done(err, file) {
-      t.ifError(err, 'should not fail (' + fixture.name + ')')
-
-      if (fixture.output) {
-        t.equal(
-          String(file),
-          String(fixture.output),
-          'should stringify ' + fixture.name
-        )
-      }
-    }
-
-    function plugin() {
-      return transform
-    }
-
-    function transform(tree) {
-      fixture.test(t, zone, tree)
-    }
-  }
 })
