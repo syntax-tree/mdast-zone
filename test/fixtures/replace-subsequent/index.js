1ff35215d6ed4752e86806e1ca79ff446468aa94
anatolykopyl <hi@kopyl.dev>
2023-11-29 01:47:13 +0300

    Fixed handling of subsequent zones

---
+++ test/fixtures/replace-subsequent/index.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ */
+
+import {zone} from 'mdast-zone'
+
+/**
+ * @param {Root} tree
+ */
+export default function assertion(tree) {
+  zone(tree, 'foo', function () {
+    return [
+      {
+        type: 'paragraph',
+        children: [{type: 'text', value: 'Bar.'}]
+      }
+    ]
+  })
+}
